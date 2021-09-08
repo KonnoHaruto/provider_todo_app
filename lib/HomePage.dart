5d40c0773b3aeba2e1933c9391331053f99785c5
Haruto Konno <qingdoujinye@gmail.com>
2021-09-08 21:36:28 +0900

    静的解析を追加

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,6 +5,13 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+      ),
+      body: Center(
+        child: Text('This is simple Widget'),
+      ),
+    );
   }
 }
