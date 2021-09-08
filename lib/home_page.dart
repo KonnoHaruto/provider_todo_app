1f4bf06b2f1178849cf478f3a719085c175aa6d2
Haruto Konno <qingdoujinye@gmail.com>
2021-09-08 21:43:45 +0900

    静的解析に対応

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo App'),
+        title: const Text('Todo App'),
       ),
-      body: Center(
-        child: Text('This is simple Widget'),
+      body: const Center(
+        child: Text('this is simple widget'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider_todo_app/HomePage.dart';
+import 'package:provider_todo_app/home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Provider Todo App',
       home: MyHomePage(),
