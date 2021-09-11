ac23fd90b7824c59a8dbdb225e2a6d768c9b85e8
Haruto Konno <qingdoujinye@gmail.com>
2021-09-11 10:08:11 +0900

    viewを追加

--- lib/view/home_page_view.dart
+++ lib/view/home_page_view.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider_todo_app/view_model/home_page_view_model.dart';
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final state = watch(todoListProvider);
+    final viewModel = watch(todoListProvider.notifier);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Provider Todo App'),
-        ),
-        body: const Center(),
+      appBar: AppBar(
+        title: const Text('Provider Todo App'),
+      ),
+      body: ListView.builder(itemBuilder: (BuildContext context, int index) {{{}}
+        return const ListTile();
+      }),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+      ));
   }
 }
