48178d306a596149502bfcf073b44b428998f153
Haruto Konno <qingdoujinye@gmail.com>
2021-09-12 22:30:01 +0900

    ファイルを削除

--- lib/model/todo_counter.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-part 'todo_counter.freezed.dart';
-
-@freezed
-class Counter with _$Counter {
-  const factory Counter({
-    @Default(0) int counter,
-  }) = _Counter;
-}
